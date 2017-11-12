1a021190b24f9347e4aad65376e432d93be97989
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2017-11-12 03:11:03 -0500

    ADD areas models

---
+++ apps/areas/models.py
@@ -0,0 +1,66 @@
+# Python imports
+
+
+# Django imports
+from django.db import models
+
+
+# Third party apps imports
+from model_utils.models import TimeStampedModel
+
+
+# Local imports
+from ..accounts.models import Account
+
+
+# Create your models here.
+class ProtectedNaturalArea(TimeStampedModel):
+    account = models.OneToOneField(
+        'Account', related_name='protected_natural_area')
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Visits(TimeStampedModel):
+    approved = models.BooleanField(default=False)
+    date = models.DateField()
+    exonerated = models.PositiveIntegerField()
+    foreign = models.PositiveIntegerField()
+    national = models.PositiveIntegerField()
+    non_paying = models.PositiveIntegerField()
+    payers = models.PositiveIntegerField()
+    protected_natural_area = models.ForeignKey('ProtectedNaturalArea')
+
+
+class Money(TimeStampedModel):
+    JANUARY = 'jan'
+    FEBRUARY = 'feb'
+    MARCH = 'mar'
+    APRIL = 'apr'
+    MAY = 'may'
+    JUNE = 'jun'
+    JULY = 'jul'
+    AUGUST = 'aug'
+    SEPTEMBER = 'sep'
+    OCTOBER = 'oct'
+    NOVEMBER = 'nov'
+    DECEMBER = 'dic'
+
+    MONTH_CHOICES = (
+        (JANUARY, 'enero'),
+        (FEBRUARY, 'febrero'),
+        (MARCH,'marzo'),
+        (APRIL, 'abril'),
+        (MAY, 'mayo'),
+        (JUNE, 'junio'),
+        (JULY, 'julio'),
+        (AUGUST, 'agosto'),
+        (SEPTEMBER, 'septiembre'),
+        (OCTOBER, 'octubre'),
+        (NOVEMBER, 'noviembre'),
+        (DECEMBER, 'diciembre'),
+    )
+
+    charge = models.ForeignKey(
+        'Account', limit_choices_to={'level': Account.ADMIN})
+    month = models.CharField(max_length=3, choices=MONTH_CHOICES)
+    mount = models.DecimalField(decimal_places=2, max_digits=10)
---
+++ apps/areas/viewsets.py
@@ -0,0 +1,14 @@
+# Python imports
+
+
+# Django imports
+
+
+# Third party apps imports
+from rest_framework.viewsets import ModelViewSet
+
+
+# Local imports
+
+
+# Create your viewsets here.
