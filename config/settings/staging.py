e7cd46249aae0942f423a966361db1106b5cc84e
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2017-11-11 23:34:21 -0500

    FIX configure staging settings

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -14,7 +14,18 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR.child('db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ATOMIC_TRANSACTIONS': True
     }
+}
+
+
+# Pipeline configuration
+PIPELINE = {
+    'PIPELINE_ENABLED': True,
 }
