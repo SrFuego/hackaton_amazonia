d47a07d818743fc4bcb33418f2fcddaef901b565
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2017-11-12 07:43:07 -0500

    ADD negative

--- apps/areas/views.py
+++ apps/areas/views.py
@@ -5,13 +5,12 @@
 from django.utils import timezone
 
 # Third party apps imports
-from rest_framework import authentication, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 # Local imports
-from .models import Visits
+from .models import ProtectedNaturalArea, Visits
 
 
 # Create your views here.
@@ -27,6 +26,7 @@ class ChartView(APIView):
         national = sum(visits.values_list('national', flat=True))
         exonerated = sum(visits.values_list('exonerated', flat=True))
         total = foreign + national + exonerated
+        exonerated_percent = round(exonerated * 100 / total) if total else 0
         return Response({
             'total': total,
             'foreign': foreign,
@@ -34,7 +34,7 @@ class ChartView(APIView):
             'exonerated': exonerated,
             'foreign_percent': round(foreign * 100 / total) if total else 0,
             'national_percent': round(national * 100 / total) if total else 0,
-            'exonerated_percent': round(exonerated * 100 / total) if total else 0,
+            'exonerated_percent': exonerated_percent,
         })
 
 
@@ -44,14 +44,53 @@ class ChartPayersView(APIView):
         if request.query_params.__len__() == 0:
             visits = Visits.objects.filter(date__month=timezone.now().month)
         else:
-            visits = Visits.objects.filter(date__month=request.query_params['month'])
+            visits = Visits.objects.filter(
+                date__month=request.query_params['month'])
         payers = sum(visits.values_list('payers', flat=True))
         non_paying = sum(visits.values_list('non_paying', flat=True))
         total = payers + non_paying
+        non_paying_percent = round(non_paying * 100 / total) if total else 0
         return Response({
             'total': total,
             'payers': payers,
             'non_paying': non_paying,
             'payers_percent': round(payers * 100 / total) if total else 0,
-            'non_paying_percent': round(non_paying * 100 / total) if total else 0,
+            'non_paying_percent': non_paying_percent,
         })
+
+
+class VisitsAnualView(APIView):
+    def get(self, request, format=None):
+        aux = {}
+        aux_list = []
+        this_year = timezone.now().year
+        last_year = this_year - 1
+        for anp in ProtectedNaturalArea.objects.all():
+            visits = Visits.objects.filter(
+                date__year=this_year, protected_natural_area=anp)
+            visits_last_year = Visits.objects.filter(
+                date__year=last_year, protected_natural_area=anp)
+            if request.query_params.__len__() == 0:
+                visits = visits.filter(date__month=timezone.now().month)
+                visits_last_year = visits_last_year.filter(
+                    date__month=timezone.now().month)
+            else:
+                visits = visits.filter(
+                    date__month=request.query_params['month'])
+                visits_last_year = visits_last_year.filter(
+                    date__month=request.query_params['month'])
+            aux['anp'] = anp.name
+            aux[str(this_year)] = sum(visits.values_list('payers', flat=True))
+            aux[str(this_year)] += sum(
+                visits.values_list('non_paying', flat=True))
+            aux[str(last_year)] = sum(
+                visits_last_year.values_list('payers', flat=True))
+            aux[str(last_year)] += sum(
+                visits_last_year.values_list('non_paying', flat=True))
+            aux_percent = aux[str(this_year)] * 100 / aux[str(last_year)]
+            aux['percent'] = aux_percent - 100 if aux[str(last_year)] else 0
+            aux['negative'] = False if aux['percent'] > 0 else True
+            aux_list.append(aux)
+            aux = {}
+
+        return Response({'list_anp': aux_list})
