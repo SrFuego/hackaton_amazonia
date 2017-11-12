b0d05c67cf1f477e6f4710eb05839d898d5806a8
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2017-11-12 00:25:33 -0500

    UPDATE permissions to any

--- config/settings/common.py
+++ config/settings/common.py
@@ -121,7 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Django Rest Framework Settings
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',)
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',)
 }
 
 # Django Rest Framework CORS configuration
