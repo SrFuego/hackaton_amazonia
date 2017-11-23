b68dd605d0c433c932c19e9aafbc6976f5a425f5
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2017-11-22 21:07:24 -0500

    ADD some test

--- apps/accounts/tests/test_models.py
+++ apps/accounts/tests/test_models.py
@@ -6,9 +6,22 @@ from django.test import TestCase
 
 
 # Third party apps imports
+from model_mommy import mommy
 
 
 # Local imports
+from ..models import Account
 
 
 # Create your model tests here.
+class AccountTestCase(TestCase):
+    def setUp(self):
+        self.account = mommy.make(Account)
+
+    def test_method_str_return_user_full_name(self):
+        self.assertEqual(
+            self.account.__str__(),
+            self.account.user.get_full_name())
+
+    def tearDown(self):
+        self.account.delete()
